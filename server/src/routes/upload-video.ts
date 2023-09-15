be21e93c68b97e6ec7123a45ec9156c2559056cb
Renan Gustavo <renandevfullstack@gmail.com>
2023-09-14 21:29:43 -0300

    backend completed

--- server/src/routes/upload-video.ts
+++ server/src/routes/upload-video.ts
@@ -25,7 +25,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
         }
 
         const extension = path.extname(data.filename);
-        if(extension === ".mp3"){
+        if(extension !== ".mp3"){
             return reply.status(400).send({ error: "Invalid input type, please upload a MP3." });
         }
 
@@ -34,5 +34,14 @@ export async function uploadVideoRoute(app: FastifyInstance) {
         const uploadDestination = path.resolve(__dirname, "../../temp", fileUploadName);
 
         await pump(data.file, fs.createWriteStream(uploadDestination));
+
+        const video = await prisma.video.create({
+            data: {
+                name: data.filename,
+                path: uploadDestination
+            }
+        });
+
+        return { video };
     });
 }
