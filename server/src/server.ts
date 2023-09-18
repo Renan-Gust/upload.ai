7a2e5d878b9d67139ae706031279000dd2f713c0
Renan Gustavo <renandevfullstack@gmail.com>
2023-09-18 11:38:33 -0300

    vercel.json headers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import { fastify } from "fastify";
-import fastifyCors from "@fastify/cors";
+import { fastifyCors } from "@fastify/cors";
 
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
